556d3965a6f7e4ef990ffc765e5f49ad4791d1d2
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-01-25 22:16:02 +0600

    solve leetcode problem Remove Element

--- Remove Element/demo.cpp
+++ Remove Element/demo.cpp
@@ -44,3 +44,17 @@ Constraints:
 0 <= nums.length <= 100
 0 <= nums[i] <= 50
 0 <= val <= 100
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
