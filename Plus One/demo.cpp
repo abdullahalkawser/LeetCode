1962d390d181e5e616844859434c7ace03c235f5
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-01-26 23:06:54 +0600

    solve problem leetcode Plus One

--- Plus One/demo.cpp
+++ Plus One/demo.cpp
@@ -32,3 +32,27 @@ Constraints:
 1 <= digits.length <= 100
 0 <= digits[i] <= 9
 digits does not contain any leading 0's.
+
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] + 1 != 10) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+            if (i == 0) {
+                digits.insert(digits.begin(), 1);
+                return digits;
+            }
+        }
+        return digits;        
+    }
+};
